8d5536ffb1acf379e3f5c2c664a4401cec8028ce
ddsakura <ddsakura@gmail.com>
2013-07-30 22:35:17 +0800

    htmlmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,37 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-        files: ['*.js']
-    },
-    uglify: {
-        build: {
-            files: {
-                'build/hello.min.js': ['src/hello.js']
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            files: ['*.js']
+        },
+        uglify: {
+            build: {
+                files: {
+                    'build/hello.min.js': ['src/hello.js']
+                }
+            }
+        },
+        htmlmin: {                                     // Task
+            dist: {                                      // Target
+                options: {                                 // Target options
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {                                   // Dictionary of files
+                    'build/hello.html': 'src/hello.html'     // 'destination': 'source'
+                }
             }
         }
-    }
-  });
+    });
 
     // Load the plugin that provides the "jshint" task.
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'uglify', 'htmlmin']);
 
 };
